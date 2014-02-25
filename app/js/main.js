b10f1c91db66ec24bd089afdaa9568f66f7f4e4e
Paul Christophe <paulchristophe@Pauls-MacBook-Air.local>
2014-02-25 13:59:37 -0500

    the bubbles are now floating around

--- app/js/main.js
+++ app/js/main.js
@@ -6,45 +6,162 @@ require({
     'vendor/three'
 ], function(THREE) {
 
+
+var container;
+
 var camera, scene, renderer;
-var geometry, material, mesh;
+var cameraCube, sceneCube;
+
+var mesh, lightMesh, geometry;
+var spheres = [];
+
+var directionalLight, pointLight;
+
+var mouseX = 0, mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 init();
 animate();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 1000;
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
+    camera.position.z = 3200;
+
+    cameraCube = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
 
     scene = new THREE.Scene();
+    sceneCube = new THREE.Scene();
 
-    geometry = new THREE.CubeGeometry(200, 200, 200);
-    material = new THREE.MeshBasicMaterial({
-        color: 0xff0000,
-        wireframe: true
-    });
+    var geometry = new THREE.SphereGeometry( 100, 32, 16 );
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    var path = "img/";
+    var format = '.jpg';
+    var urls = [
+        path + 'px' + format, path + 'nx' + format,
+        path + 'py' + format, path + 'ny' + format,
+        path + 'pz' + format, path + 'nz' + format
+    ];
 
-    renderer = new THREE.CanvasRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    var textureCube = THREE.ImageUtils.loadTextureCube( urls, new THREE.CubeRefractionMapping() );
+    var material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube, refractionRatio: 0.95 } );
 
-    document.body.appendChild(renderer.domElement);
+    for ( var i = 0; i < 500; i ++ ) {
+
+        var mesh = new THREE.Mesh( geometry, material );
+
+        mesh.position.x = Math.random() * 10000 - 5000;
+        mesh.position.y = Math.random() * 10000 - 5000;
+        mesh.position.z = Math.random() * 10000 - 5000;
+
+        mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
+
+        scene.add( mesh );
+
+        spheres.push( mesh );
+
+    }
+
+//     // Skybox
+
+//     var shader = THREE.ShaderLib[ "cube" ];
+//     shader.uniforms[ "tCube" ].value = textureCube;
+
+//     var material = new THREE.ShaderMaterial( {
+
+//         fragmentShader: shader.fragmentShader,
+//         vertexShader: shader.vertexShader,
+//         uniforms: shader.uniforms,
+//         depthWrite: false,
+//         side: THREE.BackSide
+
+//     } ),
+
+    mesh = new THREE.Mesh( new THREE.BoxGeometry( 100, 100, 100 ), material );
+    sceneCube.add( mesh );
+
+//     //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.autoClear = false;
+    container.appendChild( renderer.domElement );
+
+//     //
+
+//     window.addEventListener( 'resize', onWindowResize, false );
 
 }
+
+function onWindowResize() {
+
+//     windowHalfX = window.innerWidth / 2,
+//     windowHalfY = window.innerHeight / 2,
+
+//     camera.aspect = window.innerWidth / window.innerHeight;
+//     camera.updateProjectionMatrix();
+
+//     cameraCube.aspect = window.innerWidth / window.innerHeight;
+//     cameraCube.updateProjectionMatrix();
+
+//     renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove(event) {
+
+//     mouseX = ( event.clientX - windowHalfX ) * 10;
+//     mouseY = ( event.clientY - windowHalfY ) * 10;
+
+}
+
+// //
 
 function animate() {
 
-    // note: three.js includes requestAnimationFrame shim
-    requestAnimationFrame(animate);
+    requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.1;
-    mesh.rotation.y += 0.001;
-
-    renderer.render(scene, camera);
+    render();
 
 }
 
+function render() {
+
+    var timer = 0.0001 * Date.now();
+
+    for ( var i = 0, il = spheres.length; i < il; i ++ ) {
+
+        var sphere = spheres[ i ];
+
+        sphere.position.x = 5000 * Math.cos( timer + i );
+        sphere.position.y = 5000 * Math.sin( timer + i * 1.1 );
+
+    }
+
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+
+    camera.lookAt( scene.position );
+    cameraCube.rotation.copy( camera.rotation );
+
+    renderer.render( sceneCube, cameraCube );
+    renderer.render( scene, camera );
+
+}
+
+
 });
+
+
+
+
+
+
+
